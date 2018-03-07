c262e5d6624d751a2f2fe5ef39ba667631424004
Elizabeth Nenniger <enennige@e1z1r4p36.42.us.org>
2018-03-07 12:02:31 -0800

    added test case for solving a square

--- solve.h
+++ solve.h
@@ -6,7 +6,7 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 21:07:38 by jpollore          #+#    #+#             */
-/*   Updated: 2018/03/07 11:24:59 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/03/07 12:01:25 by enennige         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,4 +17,6 @@
 void	print_usage_msg(const char *prog_name);
 int		solve(const char *filename);
 int		get_next_point(t_point **point, char **square);
+int		solve_a_square(t_square *square, t_list **block);
+
 #endif
