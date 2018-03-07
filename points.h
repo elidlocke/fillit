f1820d4ce041da16283fc334e2468f0717f477d0
Elizabeth Nenniger <enennige@e1z1r4p36.42.us.org>
2018-03-07 09:32:10 -0800

    function to make a point from an x and y value

--- points.h
+++ points.h
@@ -6,7 +6,7 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/05 10:25:07 by jpollore          #+#    #+#             */
-/*   Updated: 2018/03/05 12:04:02 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/03/07 09:30:09 by enennige         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,7 @@ typedef struct	s_point
 	int			y;
 }				t_point;
 t_point			*create_point(int len);
+t_point			*create_point(int x, int y);
 void			free_point(t_point **point);
 void			free_minmax_points(t_point ***points);
 #endif
