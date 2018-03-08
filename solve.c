abcdc92496660e471bc395f9d4e5a75dfd610726
Elizabeth Nenniger <enennige@e1z1r4p36.42.us.org>
2018-03-08 10:51:56 -0800

    norm and free mem in solve

--- solve.c
+++ solve.c
@@ -6,7 +6,7 @@
 /*   By: jpollore <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/06 18:40:38 by jpollore          #+#    #+#             */
-/*   Updated: 2018/03/08 10:41:11 by enennige         ###   ########.fr       */
+/*   Updated: 2018/03/08 10:51:09 by enennige         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,42 +35,30 @@ int	get_next_point(t_point **point, char **square)
 	return (0);
 }
 
-int solve_a_square(t_square *square, t_list **block, t_point *p_start)
+int	solve_a_square(t_square *square, t_list **block, t_point *p_start)
 {
 	int found;
-	/* t_point *tmp; */
 
 	found = 0;
 	if (!block || !*block)
 	{
-		ft_putstrarr(square->rows);
 		return (1);
 	}
-	// FIND XY
-	while ((!(found = check_tetrimino_fits((t_tetri *)(*block)->content, square, p_start)) &&
-					get_next_point(&p_start, square->rows)))
+	while ((!(found = check_tetrimino_fits((t_tetri *)(*block)->content,
+			square, p_start)) && get_next_point(&p_start, square->rows)))
 		;
-	// CANT PLACE A BLOCK
 	if (!found)
 		return (0);
-	// PLACE A BLOCK
 	place_a_tetrimino((t_tetri *)(*block)->content, square, p_start);
-	// RECURSE
 	if (!solve_a_square(square, &((*block)->next), create_point(0)))
 	{
-		// UNDO THE LAST PLACEMENT
 		remove_a_tetrimino((t_tetri *)(*block)->content, square);
-		/* tmp = create_point_fromxy(p_start->x, p_start->y); */
-		// CALL GET NEXT POINT
 		if (!get_next_point(&p_start, square->rows))
 		{
 			p_start->x = 0;
 			p_start->y = 0;
 			return (0);
-			/* return (solve_a_square(square, block,; */
 		}
-		/* free_point(&tmp); */
-		// continue
 		return (solve_a_square(square, block, p_start));
 	}
 	return (1);
@@ -97,10 +85,13 @@ int	solve_squares(t_list **tetri_lst)
 		p_start->x = 0;
 		p_start->y = 0;
 	}
+	ft_putstrarr(square->rows);
+	free_square(&square);
+	free(square);
 	return (1);
 }
 
-int solve(const char *filename)
+int	solve(const char *filename)
 {
 	t_list		*tetri_lst;
 
