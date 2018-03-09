b6ce4fcab3a27d9bf13d80ee058bfa09c0de5603
Elizabeth Nenniger <enennige@e1z1r4p36.42.us.org>
2018-03-08 20:34:19 -0800

    normed and commented the main file

--- main.c
+++ main.c
@@ -6,23 +6,26 @@
 /*   By: enennige <enennige@student.42.us.or>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/02 15:47:02 by enennige          #+#    #+#             */
-/*   Updated: 2018/03/06 21:08:49 by jpollore         ###   ########.fr       */
+/*   Updated: 2018/03/08 20:33:55 by enennige         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-//move this to the header
 
 #include "libft.h"
 #include "solve.h"
 
+/*
+** The function print_usage_msg() prints a usage message in case users of the
+** program need a prompt on how to use it.
+*/
+
 void	print_usage_msg(const char *prog_name)
 {
-		ft_putstr_fd("usage: ", STDERR_FILENO);
-		ft_putstr_fd(prog_name, STDERR_FILENO);
-		ft_putstr_fd(" [input_file]\n", STDERR_FILENO);
+	ft_putstr_fd("usage: ", STDERR_FILENO);
+	ft_putstr_fd(prog_name, STDERR_FILENO);
+	ft_putstr_fd(" [input_file]\n", STDERR_FILENO);
 }
 
-int main(int argc, char **argv)
+int		main(int argc, char **argv)
 {
 	if (argc != 2)
 	{
